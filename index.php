c876fdda0611db3f36ca0646ad9121e76d6d4aad
Gabriel <gabriel.evan.queiroz@gmail.com>
2018-08-06 11:03:21 -0300

    config admin template

--- index.php
+++ index.php
@@ -4,18 +4,28 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
 $app->config('debug', true);
+	
+// root
+	$app->get('/', function() {
+					
+		$page = new Page();
 
-$app->get('/', function() {
-    
-	$page = new Page();
+		$page->setTpl("index");
 
-	$page->setTpl("index");
+	});
+// admin
+	$app->get('/admin', function() {
+					
+		$page = new PageAdmin();
 
-});
+		$page->setTpl("index");
+
+	});
 
 $app->run();
 
